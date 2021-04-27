f7ed5b5c4f4134edfca207ce63edb1ee736cb5ab
C Dhivya Bharathi <67786951+CDhivyaBharathi@users.noreply.github.com>
2021-04-27 18:38:53 +0530

    Update Pro109.py

--- Pro109.py
+++ Pro109.py
@@ -30,3 +30,11 @@ lst_of_data_within_3_std_deviation = [result for result in lst if result > math_
 print("{}% of data for math score lies within 1 standard deviation".format(len(lst_of_data_within_1_std_deviation)*100.0/len(lst)))
 print("{}% of data for math score lies within 2 standard deviations".format(len(lst_of_data_within_2_std_deviation)*100.0/len(lst)))
 print("{}% of data for math score lies within 3 standard deviations".format(len(lst_of_data_within_3_std_deviation)*100.0/len(lst)))
+
+fig = ff.create_distplot([data], ["reading scores"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN")) 
+fig.add_trace(go.Scatter(x=[math_first_std_deviation_start, math_first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1")) 
+fig.add_trace(go.Scatter(x=[math_first_std_deviation_end, math_first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1")) 
+fig.add_trace(go.Scatter(x=[math_second_std_deviation_start, math_second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2")) 
+fig.add_trace(go.Scatter(x=[math_second_std_deviation_end, math_second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
+fig.show()
